60d7e0fdf5100efeab1b758d5de80bd5c3dde2fe
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2026-06-08 14:40:39 -0300

    fix: firefox

--- trace-viewer/src/main.rs
+++ trace-viewer/src/main.rs
@@ -9,6 +9,8 @@ use app::{TraceViewer, subscription, title, update, view};
 use iced::{Font, Size};
 
 fn main() -> iced::Result {
+    init_wasm_diagnostics();
+
     iced::application(TraceViewer::new, update, view)
         .default_font(Font::DEFAULT)
         .title(title)
@@ -17,3 +19,12 @@ fn main() -> iced::Result {
         .antialiasing(true)
         .run()
 }
+
+#[cfg(target_arch = "wasm32")]
+fn init_wasm_diagnostics() {
+    console_error_panic_hook::set_once();
+    let _ = console_log::init_with_level(log::Level::Debug);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn init_wasm_diagnostics() {}
